ea7519fbeaca30b1fc1448e2728396d15834d942
solahidris <emailsolah@gmail.com>
2023-05-24 17:13:02 +0800

    added description

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-zinc-800 h-screen flex justify-center">
+    <div className="bg-zinc-800 h-screen flex justify-center text-white font-mono text-xs px-20 py-10 ">
       <img src={logo} className="App-logo" alt="logo" />
+      <p className="mx-4">Weather App in React JS</p>
+      {/* <p>-----------------------</p> */}
+      <p>build a Weather app user can enter a city name and then it displays the data related to the Weather of the city through an external API.</p>
     </div>
   );
 }
