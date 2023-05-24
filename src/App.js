8dd9508611a75057a0053121e1a4422316766b59
solahidris <emailsolah@gmail.com>
2023-05-24 23:22:41 +0800

    test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppHeader from "./components/AppHeader";
 import WorkFlow from "./components/WorkFlow";
 
 function App() {
+
   return (
     <div className="bg-zinc-800 h-screen">
       <AppHeader />
@@ -11,24 +12,21 @@ function App() {
 
       <div className="mt-5">
 
-        {/* Display API Box */}
         <div className="flex justify-center">
           <label className="bg-zinc-700 rounded px-3 py-2 border-2 border-zinc-800 ">display api</label>
         </div>
-
-        {/* Display CITY & TEMP text below */}
+        
         <div className="flex justify-center text-white font-mono text-xs mt-2 mb-10">
          <p>display city</p><p>//---//</p><p>display temp here</p>
         </div>
 
-        {/* CITY "text" Input Box */}
         <div className="flex justify-center">
           <input type="text" placeholder="city input here" />
         </div>
       </div> 
 
-    </div> //App DIV
-  );
-}
+    </div>
+  )
+};
 
 export default App;
