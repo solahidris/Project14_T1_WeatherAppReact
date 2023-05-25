3044f89536ef88c4b6b1a9f02db306277fdfa07c
solahidris <emailsolah@gmail.com>
2023-05-26 00:12:12 +0800

    added tailwindcss file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,50 +4,12 @@ module.exports = {
     "./src/App.js",
     "./src/components/AppHeader.js",
     "./src/components/WorkFlow.js",
+    "./src/components/CityInputBox.js",
+    "./src/components/SearchCityButton.js",
+    "./src/components/WeatherDisplay.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-// {"coord":{"lon":-0.1257,"lat":51.5085},
-
-// "weather":[{
-//   "id":804,
-//   "main":"Clouds",
-//   "description":"overcast clouds",
-//   "icon":"04d"}],
-
-// "base":"stations",
-
-// "main":{
-//   "temp":284.93,
-//   "feels_like":284.36,
-//   "temp_min":283.17,
-//   "temp_max":285.98,
-//   "pressure":1027,
-//   "humidity":84},
-
-// "visibility":10000,
-
-// "wind":{"speed":1.03,"deg":0},
-
-// "clouds":{"all":100},
-
-// "dt":1684990053,
-
-// "sys":{
-//   "type":2,
-//   "id":2075535,
-//   "country":"GB",
-//   "sunrise":1684986974,
-//   "sunset":1685044733},
-
-// "timezone":3600,
-
-// "id":2643743,
-
-// "name":"London",
-
-// "cod":200}
